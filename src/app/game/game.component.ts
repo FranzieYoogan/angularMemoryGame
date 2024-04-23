efa0eba79710c2fda168d9320c3894344c2b4ad5
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-04-22 21:03:47 -0300

    partLogicColors

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -27,6 +27,79 @@ export class GameComponent implements OnInit {
 
 
     }
+
+    colorsStart() {
+
+      let color1:any = 1;
+
+      let color2:any = 3;
+
+      let color3:any = 2;
+
+      let colorT1 = ""
+
+      let colorT2 = ""
+
+      let colorT3 = ""
+
+     color1 =   Math.floor((Math.random() * 4));
+
+     color2 =   Math.floor((Math.random() * 4));
+
+     color3 =   Math.floor((Math.random() * 4));
+
+        if(color1 == 1) {
+
+          colorT1 = "purple";
+          console.log("color1" , colorT1)
+        } else if(color1 == 2) {
+
+          colorT1 = "pink";
+          console.log("color1" , colorT1)
+        
+
+      } else if(color1 == 3) {
+
+        colorT1 = "black";
+        console.log("color1" , colorT1)
+      }
+
+      if(color2 == 1) {
+
+        colorT2 = "purple";
+        console.log("color2" , colorT2)
+      } else if(color2 == 2) {
+
+        colorT2 = "pink";
+        console.log("color2" , colorT2)
+      
+
+    } else if(color2 == 3) {
+
+      colorT2 = "black";
+      console.log("color2" , colorT2)
+    }
+
+
+
+    if(color3 == 1) {
+
+      colorT3 = "purple";
+      console.log("color3" , colorT3)
+    } else if(color3 == 2) {
+
+      colorT3 = "pink";
+      console.log("color3" , colorT3)
+    
+
+  } else if(color3 == 3) {
+
+    colorT3 = "black";
+    console.log("color3" , colorT3)
+  }
+
+
+    }
   
 
   
