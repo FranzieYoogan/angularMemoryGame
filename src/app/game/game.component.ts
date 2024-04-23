1025c58caafbc3cf2b15ab5c6a442ffd8f17aa2d
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-04-22 21:45:03 -0300

    logicColors2

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -30,11 +30,18 @@ export class GameComponent implements OnInit {
 
     colorsStart() {
 
+      const color11:any = document.getElementById('color1')
+
+      const color22:any = document.getElementById('color2')
+
+      const color33:any = document.getElementById('color3')
+
+
       let color1:any = 1;
 
-      let color2:any = 3;
+      let color2:any = 1;
 
-      let color3:any = 2;
+      let color3:any = 1;
 
       let colorT1 = ""
 
@@ -51,50 +58,75 @@ export class GameComponent implements OnInit {
         if(color1 == 1) {
 
           colorT1 = "purple";
+          color11.style.display= "block"
+          color11.innerHTML = "purple"
           console.log("color1" , colorT1)
-        } else if(color1 == 2) {
+        } if(color1 == 2) {
 
           colorT1 = "pink";
+          color11.style.display= "block"
+          color11.innerHTML = "pink"
           console.log("color1" , colorT1)
         
 
-      } else if(color1 == 3) {
+      } if(color1 == 3) {
 
         colorT1 = "black";
+        color11.style.display= "block"
+        color11.innerHTML = "black"
         console.log("color1" , colorT1)
       }
 
+
+
+
+
       if(color2 == 1) {
 
-        colorT2 = "purple";
+        colorT2 = "black";
+        color22.style.display= "block"
+        color22.innerHTML = "black"
         console.log("color2" , colorT2)
-      } else if(color2 == 2) {
+      } if(color2 == 2) {
 
         colorT2 = "pink";
+        color22.style.display= "block"
+        color22.innerHTML = "pink"
         console.log("color2" , colorT2)
       
 
-    } else if(color2 == 3) {
+    } if(color2 == 3) {
 
-      colorT2 = "black";
+      colorT2 = "purple";
+      color22.style.display= "block"
+      color22.innerHTML = "purple"
       console.log("color2" , colorT2)
     }
 
 
 
+
+    
+
     if(color3 == 1) {
 
-      colorT3 = "purple";
-      console.log("color3" , colorT3)
-    } else if(color3 == 2) {
-
       colorT3 = "pink";
+      color33.style.display= "block"
+      color33.innerHTML = "pink"
+      console.log("color3" , colorT3)
+    } if(color3 == 2) {
+
+      colorT3 = "black";
+      color33.style.display= "block"
+      color33.innerHTML = "purple"
       console.log("color3" , colorT3)
     
 
-  } else if(color3 == 3) {
+  } if(color3 == 3) {
 
-    colorT3 = "black";
+    colorT3 = "purple";
+    color33.style.display= "block"
+    color33.innerHTML = "black"
     console.log("color3" , colorT3)
   }
 
